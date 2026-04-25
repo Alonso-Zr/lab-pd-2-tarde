d132f1a63a182a0b7581bbbb4ce8ad3f97808f51
Martin Zavaleta <alonsoseguro27@gmail.com>
2026-04-25 18:11:46 -0500

    se añadio clase chirreo, cuaqueo y cuaqueo mudo

---
+++ src/main/java/com/mycompany/proyectopatos/Cuaqueo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectopatos;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Cuaqueo implements Cuaqueable {
+    
+    @Override
+    public void cuaquear() {
+        System.out.println("Yo soy un pato real por eso hago CUAC CUAC ");
+    }
+    
+}
